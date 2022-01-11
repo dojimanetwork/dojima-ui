d6be0c7bc072b8dd905a62123eea8c365e4c321e
Uday prasad <yvubr.business@gmail.com>
2022-01-11 21:36:40 +0530

    Added styling to Dojima CheckBox

--- packages/checkbox/lib/CheckboxGroup.tsx
+++ packages/checkbox/lib/CheckboxGroup.tsx
@@ -9,6 +9,7 @@ import Checkbox from '@mui/material/Checkbox'
 import { nanoid } from 'nanoid'
 import { FieldArray, FieldArrayRenderProps } from 'formik'
 import { CheckboxGroupComponentProps } from '@dojima-ui/types'
+import CheckTwoToneIcon from '@mui/icons-material/CheckTwoTone';
 
 function DojimaCheckboxGroup(props: CheckboxGroupComponentProps) {
   const {
@@ -23,7 +24,8 @@ function DojimaCheckboxGroup(props: CheckboxGroupComponentProps) {
     name,
     error,
     helperText,
-    className
+    className,
+    disable
   } = props
 
   const handleChange = (
@@ -61,6 +63,9 @@ function DojimaCheckboxGroup(props: CheckboxGroupComponentProps) {
                     onChange={(e) => handleChange(e, arrayHelpers)}
                     name={option}
                     size={checkBoxSize ?? 'small'}
+                    className={classes.checkBox}
+                    checkedIcon={<CheckTwoToneIcon/>}
+                    disabled={disable}
                   />
                 }
                 key={nanoid()}
@@ -84,7 +89,44 @@ function DojimaCheckboxGroup(props: CheckboxGroupComponentProps) {
 }
 
 const MuiStyles = makeStyles(() => ({
-  helperText: { color: 'red' }
+  helperText: { color: 'red' },
+  checkBox: {
+    "&.MuiCheckbox-root": {
+      width: '1.5rem',
+      height: '1.5rem',
+      borderRadius: '0.5rem',
+      boxShadow: '0.375rem 0.375rem 0.75rem 0 rgba(0, 0, 0, 0.16), -0.375rem -0.375rem 0.75rem 0 rgba(255, 255, 255, 0.04)',
+      border: 'solid 0.063rem rgba(0, 0, 0, 0.08)',
+      backgroundImage: 'linear-gradient(122deg, rgba(0, 0, 0, 0) 8%, #000 89%), linear-gradient(to bottom, black, black)',
+    },
+    "& .MuiSvgIcon-root": {
+      width: '1rem',
+      height: '1rem',
+      borderRadius: '0.313rem',
+      boxShadow: 'inset 0.125rem 0.125rem 0.188rem 0 rgba(0, 0, 0, 0.15), inset -0.125rem -0.125rem 0.25rem 0 rgba(255, 255, 255, 0.21)',
+      backgroundColor: '#8c8e8f',
+      color: '#8c8e8f'
+    },
+    "&.Mui-checked": {
+      "& .MuiSvgIcon-root": {
+        width: '1rem',
+        height: '1rem',
+        borderRadius: '0.313rem',
+        boxShadow: 'inset 0.125rem 0.125rem 0.188rem 0 rgba(0, 0, 0, 0.15), inset -0.125rem -0.125rem 0.25rem 0 rgba(255, 255, 255, 0.21)',
+        backgroundColor: '#e1aa75',
+        color: '#212426',
+      },
+    },
+    "&.Mui-disabled": {
+      opacity: 0.3,
+      backgroundImage: 'linear-gradient(122deg, rgba(0, 0, 0, 0) 8%, #000 89%), linear-gradient(to bottom, #212426, #212426)',
+      "& .MuiSvgIcon-root": {
+        boxShadow: 'inset 0.125rem 0.125rem 0.188rem 0 rgba(0, 0, 0, 0.15), inset -0.125rem -0.125rem 0.25rem 0 rgba(255, 255, 255, 0.21)',
+        backgroundColor: '#4b4e4f',
+        color: '#4b4e4f',
+      },
+    }
+  }
 }))
 
 export default DojimaCheckboxGroup
