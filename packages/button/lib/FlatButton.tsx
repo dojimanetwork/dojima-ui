0a1d8795c1891bb0ac9f716d590fc91c3d653f54
Uday prasad <yvubr.business@gmail.com>
2022-01-13 15:03:46 +0530

    Changed to material-ui atyled button

--- packages/button/lib/FlatButton.tsx
+++ packages/button/lib/FlatButton.tsx
@@ -1,50 +1,48 @@
-import * as React from "react";
-import ButtonUnstyled from "@mui/base/ButtonUnstyled";
-import { styled } from "@mui/system";
-import { ButtonComponentProps } from "../../types";
+import React from 'react'
+import Button from '@mui/material/Button'
+import { ButtonComponentProps } from '@dojima-ui/types'
+import { makeStyles } from '@mui/styles'
 
-const FlatButtonRoot = styled('button')(
-  () => `
-    width: 6rem;
-    height: 2.5rem;
-    padding: 0rem;
-    border-radius: 0.5rem;
-    border: 0rem none transparent;
-    background-color: #000;
-    color: #cfd0d0;
+const MuiStyles = makeStyles(() => ({
+  button: {
+    '&.MuiButton-root': {
+      width: '6rem',
+      height: '2.5rem',
+      padding: '0rem',
+      border: '0rem none transparent',
+      borderRadius: '0.5rem',
+      backgroundColor: '#000',
+      color: '#cfd0d0',
+      '&.Mui-disabled': {
+        boxShadow: 'none',
+        opacity: 0.5,
+      }
+    }
+  }
+}))
 
-    &:hover {
-      cursor: pointer;
-    };
-
-    &.Mui-disabled {
-      box-shadow: none;
-      opacity: 0.5;
-      cursor: not-allowed;
-    };
-  `,
-);
-
-export default function FlatButton(props: ButtonComponentProps) {
+function FlatButton(props: ButtonComponentProps) {
   const {
     text,
     onClick,
     disable,
-    className,
     fullWidth,
     buttonStyles
-  } = props;
-  
+  } = props
+
+  const classes = MuiStyles()
+
   return (
-      <ButtonUnstyled
-        fullWidth={fullWidth}
-        style={buttonStyles}
-        className={className}
-        onClick={onClick}
-        disabled={disable}
-        component={FlatButtonRoot}
-      >
-        {text}
-      </ButtonUnstyled>
-  );
+    <Button
+      fullWidth={fullWidth}
+      style={buttonStyles}
+      className={classes.button}
+      onClick={onClick}
+      disabled={disable}
+    >
+      {text}
+    </Button>
+  )
 }
+
+export default FlatButton
--- packages/button/lib/NeuButton.tsx
+++ packages/button/lib/NeuButton.tsx
@@ -1,58 +1,55 @@
-import * as React from "react";
-import ButtonUnstyled from "@mui/base/ButtonUnstyled";
-import { styled } from "@mui/system";
-import { ButtonComponentProps } from "../../types";
+import React from 'react'
+import Button from '@mui/material/Button'
+import { ButtonComponentProps } from '@dojima-ui/types'
+import { makeStyles } from '@mui/styles'
 
-const NeuButtonRoot = styled('button')(
-  () => `
-    width: 11.75rem;
-    height: 3.5rem;
-    padding: 0rem;
-    box-shadow: 0.375rem 0.375rem 0.75rem 0rem rgba(0, 0, 0, 0.16), -0.375rem -0.375rem 0.75rem 0rem rgba(255, 255, 255, 0.04);
-    border-radius: 1rem;
-    border: 0.25rem solid rgba(0, 0, 0, 0.08);
-    background-image: linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #000, #000);
-    background-origin: border-box;
-    background-clip: content-box, border-box;
-    color: #e1aa75;
+const MuiStyles = makeStyles(() => ({
+  button: {
+    '&.MuiButton-root': {
+      width: '11.75rem',
+      height: '3.5rem',
+      padding: '0rem',
+      border: '0.25rem solid rgba(0, 0, 0, 0.08)',
+      borderRadius: '1rem',
+      boxShadow: '0.375rem 0.375rem 0.75rem 0rem rgba(0, 0, 0, 0.16), -0.375rem -0.375rem 0.75rem 0rem rgba(255, 255, 255, 0.04)',
+      backgroundImage: 'linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #000, #000)',
+      backgroundOrigin: 'border-box',
+      backgroundClip: 'content-box, border-box',
+      color: '#e1aa75',
+      '&:focus': {
+        border: '0.25rem solid rgba(255, 255, 255, 0.02)',
+        backgroundImage: 'linear-gradient(100deg, #000 3%, rgba(0, 0, 0, 0) 96%), linear-gradient(to bottom, #000, #000)',
+      },
+      '&.Mui-disabled': {
+        boxShadow: 'none',
+        opacity: 0.5,
+      }
+    }
+  }
+}))
 
-    &:hover {
-      cursor: pointer;
-    };
-
-    &:focus {
-      border: 0.25rem solid rgba(255, 255, 255, 0.02);
-      background-image: linear-gradient(100deg, #000 3%, rgba(0, 0, 0, 0) 96%), linear-gradient(to bottom, #000, #000);
-    };
-
-    &.Mui-disabled {
-      box-shadow: none;
-      opacity: 0.5;
-      cursor: not-allowed;
-    };
-  `,
-);
-
-export default function NeuButton(props: ButtonComponentProps) {
+function NeuButton(props: ButtonComponentProps) {
   const {
     text,
     onClick,
     disable,
-    className,
     fullWidth,
     buttonStyles
-  } = props;
-  
+  } = props
+
+  const classes = MuiStyles()
+
   return (
-      <ButtonUnstyled
-        fullWidth={fullWidth}
-        style={buttonStyles}
-        className={className}
-        onClick={onClick}
-        disabled={disable}
-        component={NeuButtonRoot}
-      >
-        {text}
-      </ButtonUnstyled>
-  );
+    <Button
+      fullWidth={fullWidth}
+      style={buttonStyles}
+      className={classes.button}
+      onClick={onClick}
+      disabled={disable}
+    >
+      {text}
+    </Button>
+  )
 }
+
+export default NeuButton
