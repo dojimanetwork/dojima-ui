a3c566dbe834fda83b0808c901c31f1c357ee227
vijay04 <vijay.prasad@dojima.network>
2022-01-19 21:15:28 +0530

    NeuButton color change

--- packages/button/lib/NeuButton.tsx
+++ packages/button/lib/NeuButton.tsx
@@ -12,13 +12,13 @@ const MuiStyles = makeStyles(() => ({
       border: '0.25rem solid rgba(0, 0, 0, 0.08)',
       borderRadius: '1rem',
       boxShadow: '0.375rem 0.375rem 0.75rem 0rem rgba(0, 0, 0, 0.16), -0.375rem -0.375rem 0.75rem 0rem rgba(255, 255, 255, 0.04)',
-      backgroundImage: 'linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #000, #000)',
+      backgroundImage: 'linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #212426, #212426)',
       backgroundOrigin: 'border-box',
       backgroundClip: 'content-box, border-box',
       color: '#e1aa75',
       '&:focus': {
         border: '0.25rem solid rgba(255, 255, 255, 0.02)',
-        backgroundImage: 'linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #212426, #212426)',
+        backgroundImage: 'linear-gradient(100deg, #000 3%, rgba(0, 0, 0, 0) 96%), linear-gradient(to bottom, #212426, #212426)',
       },
       '&.Mui-disabled': {
         boxShadow: 'none',
