286a5074b80f631cf532dd775b3a916f575b926e
vijay04 <vijay.prasad@dojima.network>
2022-01-24 17:41:31 +0530

    Added props to NeuButton.

--- packages/button/lib/NeuButton.tsx
+++ packages/button/lib/NeuButton.tsx
@@ -4,17 +4,17 @@ import { ButtonComponentProps } from '@dojima-ui/types'
 import { makeStyles } from '@mui/styles'
 import { Theme } from '@mui/material'
 
-const MuiStyles = makeStyles<Theme, ButtonComponentProps>(() => ({
+const MuiStyles = makeStyles<Theme, NeuButtonProps>(() => ({
   button: {
     '&.MuiButton-root': {
-      width: '11.75rem',
-      height: '3.5rem',
+      width:({width}) => width ?? '11.75rem',
+      height:({height}) => height ?? '3.5rem',
       padding: '0rem',
       textTransform: 'none',
       border: '0.125rem solid rgba(0,0,0,0.08)',
       borderRadius: '1rem',
       boxShadow: '0.375rem 0.375rem 0.75rem 0rem rgba(0, 0, 0, 0.16), -0.375rem -0.375rem 0.75rem 0rem rgba(255, 255, 255, 0.04)',
-      backgroundImage: 'radial-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%)',
+      backgroundImage: 'linear-gradient(100deg, rgba(0, 0, 0, 0) 3%, #000 96%), linear-gradient(to bottom, #212426, #212426)',
       backgroundOrigin: 'border-box',
       backgroundClip: 'content-box, border-box',
       color:({color}) => color ?? '#e1aa75',
@@ -30,7 +30,10 @@ const MuiStyles = makeStyles<Theme, ButtonComponentProps>(() => ({
   }
 }))
 
-
+interface NeuButtonProps extends ButtonComponentProps{
+  width?: string;
+  height?: string;
+}
 
 function NeuButton(props: ButtonComponentProps) {
   const {
