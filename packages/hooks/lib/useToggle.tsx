5019f81f3341fe65d840f52b3f0b0b3202cd768e
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-07-06 22:21:00 +0530

    dev: pub: provided to pass boolean to toggle hook.

--- packages/hooks/lib/useToggle.tsx
+++ packages/hooks/lib/useToggle.tsx
@@ -3,13 +3,13 @@ import { Dispatch, SetStateAction, useCallback, useState } from 'react'
 // Parameter is the boolean, with default "false" value
 function useToggle(
   initialState = false
-): [boolean, () => void, Dispatch<SetStateAction<boolean>>] {
+): [boolean, (val?: boolean) => void, Dispatch<SetStateAction<boolean>>] {
   // Initialize the state
   const [state, setState] = useState(initialState)
 
   // Define and memorize toggler function in case we pass down the component,
   // This function change the boolean value to it's opposite value
-  const toggle = useCallback(() => setState((state) => !state), [])
+  const toggle = useCallback((val?: boolean) => setState((state) => val ?? !state), [])
 
   return [state, toggle, setState]
 }
